ced44c6643999fc9e44bb31c58f0c08ccc18f479
kymaloo <kymaloo54@gmail.com>
2024-01-19 17:08:20 +0100

    Deplacement perso + Recupere embleme

---
+++ Assets/Script/PickUp.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+public class PickUp : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Car"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Script/PlayerMovements.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed;
+
+    private Vector3 velocity = Vector3.zero;
+    void FixedUpdate()
+    {
+        float horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        float verticalMovement = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        MovePlayer(horizontalMovement, verticalMovement);
+    }
+
+    void MovePlayer(float _horizontalMovement, float _verticalMovement)
+    {
+        transform.position = new Vector3(transform.position.x + _horizontalMovement, transform.position.y + _verticalMovement, 0);
+    }
+}
